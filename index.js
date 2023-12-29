f5f18a3388e3f8169345802509f24a1c99366c05
dasomko <ds92ko@gmail.com>
2023-12-29 11:24:08 +0900

    feat: 이미지 확장자 추가 및 output 디렉토리 자동 생성 로직 추가

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const fs = require('fs');
 const path = require('path');
 const sharp = require('sharp');
 
-// 입력 파일 (jpg) 및 출력 파일 (webp) 경로 설정
+// 입력 파일 및 출력 파일 경로 설정
 const inputFolderPath = './input';
 const outputFolderPath = './output';
+
+// 허용된 이미지 확장자 배열
+const allowedExtensions = ['.jpg', '.png', '.jpeg', '.gif'];
+
+// 출력 폴더가 없으면 생성
+if (!fs.existsSync(outputFolderPath)) {
+  fs.mkdirSync(outputFolderPath);
+}
 
 fs.readdir(inputFolderPath, (err, files) => {
   if (err) {
@@ -12,21 +20,24 @@ fs.readdir(inputFolderPath, (err, files) => {
     return;
   }
 
-  // JPG 파일 필터링
-  const jpgFiles = files.filter(file => file.toLowerCase().endsWith('.jpg'));
+  // 지원되는 이미지 파일 필터링
+  const imageFiles = files.filter(file => {
+    const ext = path.extname(file).toLowerCase();
+    return allowedExtensions.includes(ext);
+  });
 
-  // 각 JPG 파일을 읽어 WebP로 변환
-  jpgFiles.forEach(jpgFileName => {
-    const jpgFilePath = path.join(inputFolderPath, jpgFileName);
-    const webpFileName = jpgFileName.replace('.jpg', '.webp');
+  // 각 이미지 파일을 읽어 WebP로 변환
+  imageFiles.forEach(imageFileName => {
+    const imageFilePath = path.join(inputFolderPath, imageFileName);
+    const webpFileName = path.basename(imageFileName, path.extname(imageFileName)) + '.webp';
     const webpFilePath = path.join(outputFolderPath, webpFileName);
 
-    sharp(jpgFilePath)
+    sharp(imageFilePath)
       .toFile(webpFilePath, (conversionErr, info) => {
         if (conversionErr) {
-          console.error(`이미지 변환 오류 (${jpgFileName}): ${conversionErr.message}`);
+          console.error(`이미지 변환 오류 (${imageFileName}): ${conversionErr.message}`);
         } else {
-          console.log(`이미지 변환 성공 (${jpgFileName}). 출력 파일: ${webpFilePath}`);
+          console.log(`이미지 변환 성공 (${imageFileName}). 출력 파일: ${webpFilePath}`);
           console.log(`변환된 이미지 정보:`, info);
         }
       });
